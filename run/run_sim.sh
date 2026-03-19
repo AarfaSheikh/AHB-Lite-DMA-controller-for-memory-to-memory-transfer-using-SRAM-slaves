16e15309bcdbbebbba711d42ead0c6fe8db34334
Aarfa Bano Sheikh <36572165+AarfaSheikh@users.noreply.github.com>
2026-03-19 14:57:38 -0400

    Run scripts

--- run/run_sim.sh
+++ run/run_sim.sh
@@ -7,12 +7,14 @@ echo "Creating library..."
 vlib work
 vmap work work
 
-echo "Compiling files..."
-vlog dma_defs_pkg.sv
-vlog dma_controller.sv
-vlog sram_ahb_subsystem.sv
-vlog top_level_system.sv
-vlog tb_top_level_system.sv
+echo "Compiling RTL..."
+vlog ../rtl/dma_defs_pkg.sv
+vlog ../rtl/dma_controller.sv
+vlog ../rtl/sram_ahb_subsystem.sv
+vlog ../rtl/top_level_system.sv
+
+echo "Compiling Testbench..."
+vlog ../tb/tb_top_level_system.sv
 
 echo "Running simulation..."
 vsim -c tb_top_level_system -do "run -all; quit"
